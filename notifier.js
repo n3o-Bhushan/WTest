739781febdd2631a2bbe392ce53bea10eb3fae72
Bhushan Deshmukh <bhushan.deshmukh2012@gmail.com>
2017-05-07 15:39:58 -0700

    JS Module
    Created and added a module for JS. Which can be used in multiple HTML pages.

---
+++ notifier.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+ window.addEventListener('load', function() {
+  var status = document.getElementById("stat");
+  function updateOnlineStatus(event) {
+    var condition = navigator.onLine ? "online" : "offline";
+    stat.className = condition;
+    stat.innerHTML = condition.toUpperCase();
+   
+  }
+  window.addEventListener('online',  updateOnlineStatus);
+  window.addEventListener('offline', updateOnlineStatus);
+});
+ </script>
